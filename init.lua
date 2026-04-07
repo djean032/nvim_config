c92f2565ae21143b878f6c7d7b792d1e169cb034
djean032 <djean032@gmail.com>
2026-04-07 15:35:58 -0400

    nvim config

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("config.keybinds")
+require("config.opts")
+require("config.autocmds")
+require("config.lazy")
+-- colorscheme
+vim.cmd([[colorscheme unokai]])
+
+vim.g.have_nerd_font = true
