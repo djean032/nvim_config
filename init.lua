3eda20673dab02ae7597388a26fb473b90620325
Dairen Jean <djean032@gmail.com>
2026-04-07 21:13:21 -0400

    added snacks and removed telescope

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ require("config.opts")
 require("config.autocmds")
 require("config.lazy")
 -- colorscheme
-vim.cmd([[colorscheme unokai]])
+vim.cmd([[colorscheme tokyonight]])
 
 vim.g.have_nerd_font = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
